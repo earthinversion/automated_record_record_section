92d9af70d8fef631f1eccd735488725c4f82c25d
Utpal Kumar <utpalkumar50@gmail.com>
2020-07-03 12:04:45 +0800

    performance improvements

--- README.md
+++ README.md
@@ -2,8 +2,7 @@
 - Define the input parameters in `input_file.yml`
 - Run the python script `plot_record_section.py`
 
-<img src="example_inputFile.jpg" width="400" alt="Record section 1">
+<img src="example_inputFile.jpg" width="400" alt="Input file">
 
 
-<img src="record_section_2013-05-24.png" width="400" alt="Record section 1">
-<img src="record_section_2012-01-01.png" width="400" alt="Record section 2">
+<img src="record_section_2013-05-24_SEA-OF-OKHOTSK.png" width="400" alt="Record section 1">
--- plot_record_section.py
+++ plot_record_section.py
@@ -9,9 +9,12 @@ from obspy.core import Stream
 from matplotlib.transforms import blended_transform_factory
 import warnings
 warnings.filterwarnings('ignore')
+# Utpal Kumar
+# Institute of Earth Sciences, Academia Sinica, Taipei, Taiwan
 
 def extract_catalog_info_to_txt(catalog, catalogtxt):
     '''
+    Utpal Kumar
     Writing catalog in txt file and returning pandas dataframe
     :type catalog: :class: 'obspy.core.event.catalog.Catalog'
     :param catalog: earthquake catalog
@@ -23,10 +26,6 @@ def extract_catalog_info_to_txt(catalog, catalogtxt):
         try:
             try:
                 evtime,evlat,evlon,evdp,evmg,evmgtp, evdes =cat.origins[0].time,cat.origins[0].latitude,cat.origins[0].longitude,cat.origins[0].depth/1000,cat.magnitudes[0].mag,cat.magnitudes[0].magnitude_type, cat.event_descriptions[0].text
-                # print()
-                # print("event_description: ",dir(cat))
-                # print("event_description: ",dir(cat.origins[0]))
-                # print("event_description: ",cat.event_descriptions[0].text)
             except:
                 evtime,evlat,evlon,evdp,evmg,evmgtp, evdes=cat.origins[0].time,cat.origins[0].latitude,cat.origins[0].longitude,cat.origins[0].depth/1000,cat.magnitudes[0].mag,"-9999", cat.event_descriptions[0].text
             evtimes.append(str(evtime))
@@ -54,7 +53,7 @@ starttime = inp['starttime']
 endtime = inp["endtime"]
 minmagnitude = inp["minmagnitude"]
 maxmagnitude = inp["maxmagnitude"]
-catalogxmlloc = inp["catalogxmlloc"]
+resultsloc = inp["resultsLoc"]
 client_name = inp["client_name"]
 channel = inp["channel"]
 location = inp["location"]
@@ -62,10 +61,10 @@ record_section_relative_beg_time = inp["record_section_relative_beg_time"]
 record_section_relative_end_time = inp["record_section_relative_end_time"]
 
 # defining output catalog file location and name
-catalogxml = catalogxmlloc+'events.xml'
-catalogtxt = catalogxmlloc+'events.txt'
-stationxml = 'stations.xml'
-stationtxt = 'stations.txt'
+catalogxml = resultsloc+'events.xml'
+catalogtxt = resultsloc+'events.txt'
+stationxml = resultsloc+'stations.xml'
+stationtxt = resultsloc+'stations.txt'
 
 
 
@@ -102,23 +101,24 @@ inventory = client.get_stations(startbefore=wf_starttime, endafter=wf_endtime,ne
 inventory.write(stationtxt, 'STATIONTXT',level='station')
 
 df_stationlist = pd.read_csv(stationtxt,sep="|")
-# df_stationlist = df_stationlist.iloc[16:100,:]
-dists = []
+df_stationlist = df_stationlist.iloc[16:100,:]
+
 print("--> Downloading waveforms for the event: {} @{}".format(df['eventDescrp'].values[eq_id],eq_origin_time))
 st = Stream()
 count = 0
 for ii, net, sta, lat, lon in zip(np.arange(df_stationlist.shape[0]), df_stationlist['#Network'],df_stationlist['Station'],df_stationlist['Latitude'],df_stationlist['Longitude']):
     try:
-        tr = client.get_waveforms(net, sta, location, channel, wf_starttime, wf_endtime, attach_response = True)
         dist, baz, _ = gps2dist_azimuth(lat,lon,evLat,evLon)
-        
+        if dist < inp['maxDist']:
+            tr = client.get_waveforms(net, sta, location, channel, wf_starttime, wf_endtime, attach_response = True)
+            st += tr
+            st[count].stats['distance'] = dist
 
-        st += tr
-        dists.append(dist)
-        st[count].stats['distance'] = dist
+            print(f"{ii+1}/{df_stationlist.shape[0]} Successfully downloaded trace for {net}-{sta}-{channel}, dist: {dist/1000:.2f} km")
+            count += 1
+        else:
+            print(f"{ii+1}/{df_stationlist.shape[0]} Skipped {net}-{sta}-{channel}, dist: {dist/1000:.2f} km")
 
-        print(f"{ii+1}/{df_stationlist.shape[0]} Successfully downloaded trace for {net}-{sta}-{channel}, dist: {dist/1000:.2f} km")
-        count += 1
     except:
         print(f"{ii}/{df_stationlist.shape[0]} Failed {net}-{sta}-{channel}: ({lat:.2f}, {lon:.2f})")
 
@@ -126,28 +126,33 @@ for ii, net, sta, lat, lon in zip(np.arange(df_stationlist.shape[0]), df_station
     if len(st)>=inp['max_num_trace_to_plot']:
         break
 
-# bandpass filter
-if inp['filter_trace']:
-    print("--> Bandpass filtering")
-    st.filter('bandpass', freqmin=inp['bandpass_filter']['freqmin'], freqmax=inp['bandpass_filter']['freqmax'])
+if len(st):
+    # bandpass filter
+    if inp['filter_trace']:
+        print("--> Bandpass filtering")
+        st.filter('bandpass', freqmin=inp['bandpass_filter']['freqmin'], freqmax=inp['bandpass_filter']['freqmax'])
 
-# remove instrument response
-st_rem = st.copy()
-if inp['rem_response']:
-    print("--> Removing response")
-    st_rem.remove_response(output='DISP')
+    # remove instrument response
+    st_rem = st.copy()
+    if inp['rem_response']:
+        print("--> Removing response")
+        st_rem.remove_response(output='DISP')
 
-st_rem.write(f"stream_rs_{ev_time_str}.mseed", format="MSEED")
+    st_rem.write(resultsloc+f"stream_rs_{ev_time_str}.mseed", format="MSEED")
 
-print("--> Plotting record section")
+    print("--> Plotting record section")
 
-fig = plt.figure()
-st_rem.plot(type='section',grid_linewidth=0.1,dist_degree=False, fig=fig, time_down=True)
+    fig = plt.figure()
+    st_rem.plot(type='section',grid_linewidth=0.1,dist_degree=False, fig=fig, time_down=True, offset_min = inp['offset_min'])
 
-ax = fig.axes[0]
-transform = blended_transform_factory(ax.transData, ax.transAxes)
-for tr in st_rem:
-    ax.text(tr.stats.distance / 1e3, 1.0, tr.stats.station, rotation=270,
-            va="bottom", ha="center", transform=transform, zorder=10)
-plt.title(f"Record section for {df['eventMag'].values[eq_id]} {df['magUnit'].values[eq_id]} EQ @ {df['eventDescrp'].values[eq_id]}\n\n", fontsize=14)
-plt.savefig(f"record_section_{ev_time_str}.png",dpi=300,bbox_inches='tight')
+    ax = fig.axes[0]
+    transform = blended_transform_factory(ax.transData, ax.transAxes)
+    for tr in st_rem:
+        ax.text(tr.stats.distance / 1e3, 1.0, tr.stats.station, rotation=270,
+                    va="bottom", ha="center", transform=transform, zorder=10)
+    plt.title(f"Record section for {df['eventMag'].values[eq_id]} {df['magUnit'].values[eq_id]} EQ @ {df['eventDescrp'].values[eq_id]}\n\n", fontsize=14)
+    eventDes = "-".join(df['eventDescrp'].values[eq_id].split())
+    plt.savefig(resultsloc+f"record_section_{ev_time_str}_{eventDes}.png",dpi=300,bbox_inches='tight')
+    plt.close('all')
+else:
+    print("No traces in the stream for the defined parameters")
